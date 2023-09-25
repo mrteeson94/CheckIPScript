382edf5574d7f48277be6c5dd7d1af454db4fa13
Tyson Nguyen <yngu4822@gmail.com>
2023-09-25 11:28:30 +1000

    Create CheckIPv4 Port Scripts :sparkles:

---
+++ CheckIPPort.py
@@ -0,0 +1,25 @@
+# Tyson Nguyen | tyson.nguyen@studytafensw.edu.au
+# Copyright @Red Opal Innovations
+# Proprietary License
+# Last Updated: 27/09/23
+# Version 1.0.1
+# Status 'Development'
+
+# 1. Def and set MAIN() Function to execute all logic
+# 1.1 Assign variable to take input
+# 1.2 Feed var in Validate_input's param for validation check
+# 1.3
+
+# 2.
+
+# 3.
+
+# 4.
+
+# 5.
+
+# 6.
+
+# 7.
+
+# 8.
